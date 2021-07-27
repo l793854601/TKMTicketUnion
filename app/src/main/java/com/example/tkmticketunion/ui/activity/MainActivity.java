086e35ae0d295df0e5a5ba58c60e4d7cbf3fdfe4
TAKUMI <793854601@qq.com>
2021-07-27 20:17:53 +0800

    添加底部BottomNavigationView，设置图片、文字、颜色等

--- app/src/main/java/com/example/tkmticketunion/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/tkmticketunion/ui/activity/MainActivity.java
@@ -8,7 +8,6 @@ import com.example.tkmticketunion.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
